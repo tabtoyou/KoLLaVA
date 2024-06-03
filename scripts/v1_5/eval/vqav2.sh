b4e3c760950345f2ca8d2279990d2523f382ba5e
iamiks <rmstjr1030@naver.com>
2024-06-03 12:23:45 +0900

    Fix : vqa 평가 스크립트 일부 수정

--- scripts/v1_5/eval/vqav2.sh
+++ scripts/v1_5/eval/vqav2.sh
@@ -5,19 +5,19 @@ IFS=',' read -ra GPULIST <<< "$gpu_list"
 
 CHUNKS=${#GPULIST[@]}
 
-CKPT="llava-v1.5-13b"
+CKPT="KoLLaVA-v1.5-Synatra-7b"
 SPLIT="llava_vqav2_mscoco_test-dev2015"
 
 for IDX in $(seq 0 $((CHUNKS-1))); do
     CUDA_VISIBLE_DEVICES=${GPULIST[$IDX]} python -m llava.eval.model_vqa_loader \
-        --model-path liuhaotian/llava-v1.5-13b \
+        --model-path tabtoyou/KoLLaVA-v1.5-Synatra-7b \
         --question-file ./playground/data/eval/vqav2/$SPLIT.jsonl \
         --image-folder ./playground/data/eval/vqav2/test2015 \
         --answers-file ./playground/data/eval/vqav2/answers/$SPLIT/$CKPT/${CHUNKS}_${IDX}.jsonl \
         --num-chunks $CHUNKS \
         --chunk-idx $IDX \
         --temperature 0 \
-        --conv-mode vicuna_v1 &
+        --conv-mode mistral &
 done
 
 wait
